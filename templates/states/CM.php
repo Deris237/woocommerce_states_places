9e6a16fc76d1e48c217f572c0b8ff007dc9773bd
Deris237 <134659024+Deris237@users.noreply.github.com>
2024-04-05 16:26:47 +0100

    CM.php
    Les regions du cameroun.

---
+++ templates/states/CM.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+* Regions of Cameroom
+* 10 regions
+*
+* Source:
+* - https://fr.wikipedia.org/wiki/Régions_du_Cameroun
+*
+* @author  Deris DADJO <dadjofokouang400@@gmail.com>
+* @version 1.3.3
+* @license http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+global $states;
+
+$states['CM'] = array(
+    'EN' => 'Extrême-Nord',
+    'NO' => 'Nord',
+    'AD' => 'Adamaoua',
+    'ES' => 'Est',
+    'CE' => 'Centre',
+    'SU' => 'Sud',
+    'LT' => 'Littoral',
+    'OU' => 'Ouest',
+    'NW' => 'Nord-Ouest',
+    'SW' => 'Sud-Ouest',
+);
